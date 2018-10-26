0eaf8b8f98c67cb0c38719b8184d3ffdf1efd449
Marty <marty.stepien@gmail.com>
2018-10-26 12:00:38 +0100

    - part 1: reversed name file saved

--- part-1.js
+++ part-1.js
@@ -1,1 +1,19 @@
-console.log('your code here')
+console.log('your code here');
+
+const fs = require('fs');
+const fileContent = fs.readFileSync('names.txt', 'utf-8');
+
+const arrayOfNames = fileContent.match(/[^\r\n]+/g);
+let reversedArr = [];
+
+for (var name of arrayOfNames) {
+    reversedArr.push(name);
+}
+
+joinedReversedArr = reversedArr.join(" ");
+
+fs.writeFileSync('names-reversed.txt', joinedReversedArr, 'utf-8');
+
+console.log(arrayOfNames[0]);
+console.log(reversedArr[0]);
+console.log(joinedReversedArr);
